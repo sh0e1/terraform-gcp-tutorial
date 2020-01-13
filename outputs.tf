461375f919fef3baf47df56d3ce8f3645fde45df
sh0e1 <sh0e1@users.noreply.github.com>
2020-01-14 01:19:36 +0900

    :tada: Initial Commit

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "ip" {
+  value = google_compute_address.vm_static_ip.address
+}
+
+output "vpc_network_subnets_ips" {
+  value = module.network.subnets_ips
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project" {
+  type = string
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-c"
+}
+
+variable "cidrs" {
+  type = list
+  default = []
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "machine_types" {
+  type = map
+  default = {
+    "dev" = "f1-micro"
+    "test" = "n1-highcpu-32"
+    "prod" = "n1-highcpu-32"
+  }
+}
